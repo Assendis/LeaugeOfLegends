8aecc4cd0af34bab04791cf5e7eab244bd70b1f5
Assendis <Assendis@DESKTOP-N3PNDGV>
2018-11-02 16:56:19 +0300

    Proje dosyası ekle.

---
+++ LeaugeOfLegends/Program.cs
@@ -0,0 +1,182 @@
+﻿using System;
+
+namespace LeagueOfLegends
+{
+    interface Character
+    {
+        string Name
+        {
+            get;
+            set;
+        }
+
+        int HealtyValue
+        {
+            get;
+            set;
+        }
+
+        int PowerOfAttack
+        {
+            get;
+            set;
+        }
+    }
+
+    class Warrior : Character
+    {
+        public string Name { get; set; }
+        public int HealtyValue { get; set; }
+        public int PowerOfAttack { get; set; }
+
+        public Warrior(string _name, int _healtyvalue, int _powerOfAttack)
+        {
+            Name = _name;
+            HealtyValue = _healtyvalue;
+            PowerOfAttack = _powerOfAttack;
+        }
+
+        public void PickAttackEquipment(int value)
+        {
+            PowerOfAttack += value;
+        }
+
+        public void PickHealthEquipment(int value)
+        {
+            HealtyValue += value;
+        }
+    }
+
+    class Magician : Character
+    {
+        public string Name { get; set; }
+        public int HealtyValue { get; set; }
+        public int PowerOfAttack { get; set; }
+
+        public Magician(string _name, int _healtyvalue, int _powerOfAttack)
+        {
+            Name = _name;
+            HealtyValue = _healtyvalue;
+            PowerOfAttack = _powerOfAttack;
+        }
+
+        public void PickHealthEquipment(int value)
+        {
+            HealtyValue += value;
+        }
+    }
+
+    class Support : Character
+    {
+        public string Name { get; set; }
+        public int HealtyValue { get; set; }
+        public int PowerOfAttack { get; set; }
+
+        public Support(string _name, int _healtyvalue, int _powerOfAttack)
+        {
+            Name = _name;
+            HealtyValue = _healtyvalue;
+            PowerOfAttack = _powerOfAttack;
+        }
+
+        public void PickAttackEquipment(int value)
+        {
+            PowerOfAttack += value;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string characterChoice;
+            string healthEquipmentChoice;
+            string attackEquipmentChoice;
+
+            Console.WriteLine("Pick a character!");
+            Console.WriteLine("Warrior: 1\nMagician: 2\nSupport: 3");
+
+            characterChoice = Console.ReadLine();
+
+
+            switch (characterChoice)
+            {
+                case "1":
+                    Warrior warrior = new Warrior("Jack", 100, 50);
+                    Console.WriteLine("Pick your health equipment!");
+                    Console.WriteLine("Mavi büyü(+10 HP): 1");
+                    Console.WriteLine("Yeşil büyü(+5 HP): 2");
+                    healthEquipmentChoice = Console.ReadLine();
+
+                    if (healthEquipmentChoice == "1")
+                        warrior.PickHealthEquipment(10);
+                    else if (healthEquipmentChoice == "2")
+                        warrior.PickHealthEquipment(5);
+
+                    Console.WriteLine("Pick your attack equipment!");
+                    Console.WriteLine("Kılıç(+20 XP): 1");
+                    Console.WriteLine("Silah(+50 XP): 2");
+                    attackEquipmentChoice = Console.ReadLine();
+
+                    if (attackEquipmentChoice == "1")
+                        warrior.PickAttackEquipment(20);
+                    else if (attackEquipmentChoice == "2")
+                        warrior.PickAttackEquipment(50);
+
+                    Console.WriteLine("------------------------------");
+                    Console.WriteLine("Tip           : Savaşçı");
+                    Console.WriteLine("İsim          : " + warrior.Name);
+                    Console.WriteLine("Sağlık Değeri : " + warrior.HealtyValue);
+                    Console.WriteLine("Atak Gücü     : " + warrior.PowerOfAttack);
+                    Console.ReadKey();
+
+                    break;
+
+                case "2":
+                    Magician magician = new Magician("Sofia", 65, 30);
+                    Console.WriteLine("Pick your health equipment!");
+                    Console.WriteLine("Mavi büyü(+50 HP): 1");
+                    Console.WriteLine("Yeşil büyü(+30 HP): 2");
+                    healthEquipmentChoice = Console.ReadLine();
+
+                    if (healthEquipmentChoice == "1")
+                        magician.PickHealthEquipment(50);
+                    else if (healthEquipmentChoice == "2")
+                        magician.PickHealthEquipment(30);
+
+                    Console.WriteLine("------------------------------");
+                    Console.WriteLine("Tip           : Büyücü");
+                    Console.WriteLine("İsim          : " + magician.Name);
+                    Console.WriteLine("Sağlık Değeri : " + magician.HealtyValue);
+                    Console.WriteLine("Atak Gücü     : " + magician.PowerOfAttack);
+                    Console.ReadKey();
+
+                    break;
+
+                case "3":
+                    Support support = new Support("Omni", 80, 40);
+                    Console.WriteLine("Pick your attack equipment!");
+                    Console.WriteLine("Kılıç(+10 XP): 1");
+                    Console.WriteLine("Silah(+20 XP): 2");
+                    attackEquipmentChoice = Console.ReadLine();
+
+                    if (attackEquipmentChoice == "1")
+                        support.PickAttackEquipment(10);
+                    else if (attackEquipmentChoice == "2")
+                        support.PickAttackEquipment(20);
+
+                    Console.WriteLine("------------------------------");
+                    Console.WriteLine("Tip           : Destek");
+                    Console.WriteLine("İsim          : " + support.Name);
+                    Console.WriteLine("Sağlık Değeri : " + support.HealtyValue);
+                    Console.WriteLine("Atak Gücü     : " + support.PowerOfAttack);
+                    Console.ReadKey();
+
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ LeaugeOfLegends/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("LeaugeOfLegends")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LeaugeOfLegends")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("3ca95c83-7f14-4b2c-8577-cf055ffb38cf")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// '*' kullanarak varsayılana ayarlayabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
